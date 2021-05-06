30c67b9f8f376c3c3fc70063729ed3b4f5eaf061
DESKTOP-OMCIDCD\iago_ <iago.p.oliveira97@gmail.com>
2021-05-06 14:08:33 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { data } from './data'
+import './App.css'
+import { useState } from 'react';
 
 function App() {
+  const [showSecretIdentity, setShowSecretIdentity] = useState(false)
+  const [indexId, setindexId] = useState<number>()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data.HeroesAlmanac.title}</h1>
+      <h2>{data.HeroesAlmanac.subtitle}</h2>
+      <div className="items">
+      {data.HeroesAlmanac.heroes.map((item, index) => (
+        <div key={index} onClick={() => {setShowSecretIdentity(!showSecretIdentity); setindexId(index)}} >
+          <h3>{item.heroName}</h3>
+          <img src={item.avatar} className="App-logo" alt="logo" />
+          <h4>{item.description}</h4>
+          {(showSecretIdentity && index === indexId) && <h3>{item.secretIdentity}</h3>}
+        </div>
+      ))}
+      </div>
     </div>
   );
 }
---
+++ src/data.ts
@@ -0,0 +1,30 @@
+export const data = {
+    HeroesAlmanac: {
+      title: "Super Heroes Almanac",
+      subtitle: "The ultimate guide",
+      heroes: [
+        {
+          avatar: "https://i.ibb.co/xjLzhxr/ex1-Artboard-1.jpg",
+          heroName: "Captain America",
+          secretIdentity: "Steve Rogers",
+          description:
+            "America’s World War II Super-Soldier continues his fight in the present as an Avenger and untiring sentinel of liberty."
+        },
+        {
+          avatar: "https://i.ibb.co/M93zQ17/ex1-Artboard-3.jpg",
+          heroName: "Iron Man",
+          secretIdentity: "Tony Stark",
+          description:
+            "Inventor Tony Stark applies his genius for high-tech solutions to problems as Iron Man, the armored Avenger."
+        },
+        {
+          avatar: "https://i.ibb.co/D1v60km/ex1-Artboard-2.jpg",
+          heroName: "Thor",
+          secretIdentity: "Hot guy",
+          description:
+            "Thor Odinson wields the power of the ancient Asgardians to fight evil throughout the Nine Realms and beyond."
+        }
+      ]
+    }
+  };
+
